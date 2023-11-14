a1d98fd7302ced59ab10e57f44ecc2360e33cd7a
ihongjinlee <ihongjinlee@gmail.com>
2023-11-14 17:15:29 +0900

    네비 컴포넌트 close #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <div className='text-red-900'>Hello</div>;
+  return (
+    <section className='flex h-full justify-center items-center'>
+      <h1 className='text-2xl'>수치의 합계를 보기 편하게</h1>
+    </section>
+  );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <div className='flex justify-between items-center h-[60px] px-4'>
+      <nav>
+        <ul className='text-2xl font-bold'>나의 합계 계산기</ul>
+      </nav>
+      <nav>
+        <ul>로그인</ul>
+      </nav>
+    </div>
+  );
+}
