a33aa131ee139bafc3c7b86dec9a6fcaa25ea8f3
ihongjinlee <ihongjinlee@gmail.com>
2023-11-15 18:15:47 +0900

    local, development 에서만 버전 표시 하기 #11

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import packageJson from '../../package.json';
 import { useEffect } from 'react';
 import { signOut, useSession } from 'next-auth/react';
 import Avatar from './Avatar';
@@ -37,9 +38,12 @@ export default function Navbar() {
     <div className='flex justify-between items-center h-[60px] px-4'>
       <nav className='flex'>
         <div className='text-2xl font-bold'>나의 합계 계산기</div>
-        <div className='flex items-end text-gray-500'>
-          {process.env.NEXT_PUBLIC_RUN_MODE}
-        </div>
+        {(process.env.NEXT_PUBLIC_RUN_MODE === 'local' ||
+          process.env.NEXT_PUBLIC_RUN_MODE === 'development') && (
+          <div className='flex items-end text-gray-500 text-sm`'>
+            {process.env.NEXT_PUBLIC_RUN_MODE} v{packageJson.version}
+          </div>
+        )}
       </nav>
       <nav>
         <ul className='flex gap-2 items-center'>
