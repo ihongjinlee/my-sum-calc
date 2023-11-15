7ef56c3d44e6d466f9f5b2954ccebebf3be6f009
ihongjinlee <ihongjinlee@gmail.com>
2023-11-15 16:47:29 +0900

    Navbar에 NEXT_PUBLIC_RUN_MODE 표시 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,8 +35,11 @@ export default function Navbar() {
 
   return (
     <div className='flex justify-between items-center h-[60px] px-4'>
-      <nav>
-        <ul className='text-2xl font-bold'>나의 합계 계산기</ul>
+      <nav className='flex'>
+        <div className='text-2xl font-bold'>나의 합계 계산기</div>
+        <div className='flex items-end text-gray-500'>
+          {process.env.NEXT_PUBLIC_RUN_MODE}
+        </div>
       </nav>
       <nav>
         <ul className='flex gap-2 items-center'>
